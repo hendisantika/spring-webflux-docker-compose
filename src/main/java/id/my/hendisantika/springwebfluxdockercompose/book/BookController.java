fd60e3e1d20e44406a90b33025a1f871479b871c
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 08:03:51 +0700

    Add save in BookController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springwebfluxdockercompose/book/BookController.java
+++ src/main/java/id/my/hendisantika/springwebfluxdockercompose/book/BookController.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.springwebfluxdockercompose.book;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -28,5 +32,12 @@ public record BookController(BookService bookService) {
     @GetMapping
     public Flux<Book> findAll() {
         return bookService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Book> save(@RequestBody Book payload) {
+        Book book = new Book(null, payload.title(), payload.isbn());
+        return bookService.save(book);
     }
 }
