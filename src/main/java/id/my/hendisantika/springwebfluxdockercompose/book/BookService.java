ce8bf589ec5df9e1a599846a9b6e587d70ac7fdc
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 08:01:51 +0700

    Add findAll in BookService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springwebfluxdockercompose/book/BookService.java
+++ src/main/java/id/my/hendisantika/springwebfluxdockercompose/book/BookService.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.springwebfluxdockercompose.book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
@@ -30,6 +31,17 @@ public class BookService {
                         .flatMap(book -> reactiveRedisTemplate.opsForValue()
                                 .set(key, book, Duration.ofMinutes(5))
                                 .thenReturn(book))
+                );
+    }
+
+    public Flux<Book> findAll() {
+        return reactiveRedisTemplate.keys("book:*")
+                .flatMap(key -> reactiveRedisTemplate.opsForValue().get(key))
+                .switchIfEmpty(bookRepository.findAll()
+                        .flatMap(book -> reactiveRedisTemplate.opsForValue()
+                                .set("book:" + book.id(), book, Duration.ofMinutes(5)))
+                        .thenMany(reactiveRedisTemplate.keys("book:*")
+                                .flatMap(key -> reactiveRedisTemplate.opsForValue().get(key)))
                 );
     }
 }
