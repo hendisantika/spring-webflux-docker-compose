38805dc1d132c0ae43dc811452fa77a9db854db9
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 08:04:05 +0700

    Add deleteById in BookController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springwebfluxdockercompose/book/BookController.java
+++ src/main/java/id/my/hendisantika/springwebfluxdockercompose/book/BookController.java
@@ -1,6 +1,7 @@
 package id.my.hendisantika.springwebfluxdockercompose.book;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,10 @@ public record BookController(BookService bookService) {
     public Mono<Book> save(@RequestBody Book payload) {
         Book book = new Book(null, payload.title(), payload.isbn());
         return bookService.save(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteById(@PathVariable Long id) {
+        return bookService.deleteById(id).then();
     }
 }
