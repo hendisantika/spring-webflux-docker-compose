839387b49196236caf0f585f98553477b450184a
hendisantika <hendisantika@yahoo.co.id>
2024-03-16 07:54:06 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-webflux-docker-compose
---
+++ src/main/java/id/my/hendisantika/springwebfluxdockercompose/SpringWebfluxDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springwebfluxdockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxDockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxDockerComposeApplication.class, args);
+    }
+
+}
