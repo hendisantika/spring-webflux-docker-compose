dfa688a989770d5bd0a4b1aa00f36f81ab3ed5ce
hendisantika <hendisantika@yahoo.co.id>
2024-05-06 09:58:42 +0700

    Add more details in README.md file :two:🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springwebfluxdockercompose/config/RedisConfig.java
+++ src/main/java/id/my/hendisantika/springwebfluxdockercompose/config/RedisConfig.java
@@ -1,13 +1,12 @@
 package id.my.hendisantika.springwebfluxdockercompose.config;
 
+import id.my.hendisantika.springwebfluxdockercompose.book.Book;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-
-import java.awt.print.Book;
 
 /**
  * Created by IntelliJ IDEA.
